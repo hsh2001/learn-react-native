59bd735684207dafaf7a57934238832bfe40e192
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2019-10-28 13:22:19 +0900

    Add Button.

--- rn-first-app/App.js
+++ rn-first-app/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('황승현');
   return (
     <View style={styles.container}>
-      <Text>Hello world!</Text>
+      <Text>{text}</Text>
+      <Button title="누구세요?" onPress={() => setText("개발자")} />
     </View>
   );
 }
