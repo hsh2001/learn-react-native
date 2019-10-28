f69ac18fc9345065a86da49252932b0d5947e4c6
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2019-10-28 13:40:03 +0900

    Add darkmode.

--- rn-first-app/App.js
+++ rn-first-app/App.js
@@ -3,10 +3,19 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   const [text, setText] = useState('황승현');
+  const [darkmode, setDarkmode] = useState(false);
   return (
-    <View style={styles.container}>
-      <Text>{text}</Text>
-      <Button title="누구세요?" onPress={() => setText("개발자")} />
+    <View style={{
+      ...styles.container,
+      backgroundColor: (darkmode? '#000' : '#fff'),
+    }}>
+      <Text style={{color: darkmode? '#fff' : '#000' }}>
+        {text}
+      </Text>
+      <Button title="누구세요?"
+              onPress={() => setText("개발자")} />
+      <Button title={`다크모드 ${darkmode? 'off' : 'on'}`}
+              onPress={() => setDarkmode(!darkmode)} />
     </View>
   );
 }
@@ -14,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    color: '#f00',
   },
 });
